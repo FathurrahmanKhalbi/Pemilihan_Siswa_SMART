8c598dd784de7ef0d95618c7f134ccaba254a0d8
FathurrahmanKhalbi <162527054+FathurrahmanKhalbi@users.noreply.github.com>
2025-10-07 11:53:08 +0700

    Create hapus_alt.php

---
+++ hapus_alt.php
@@ -0,0 +1,27 @@
+<?php
+include("koneksi.php");
+
+if (isset($_GET['id_alt'])) {
+    $id = $_GET['id_alt'];
+
+    $sqlDeleteRelatedData = "DELETE FROM tabel_nilai WHERE alternatif_id = $id";
+    $resultRelatedData = mysqli_query($dbcon, $sqlDeleteRelatedData);
+
+    if ($resultRelatedData) {
+        $sql = "DELETE FROM alternatif WHERE id_alt = $id";
+        $result = mysqli_query($dbcon, $sql);
+
+        if ($result) {
+            echo "Record deleted successfully";
+            header("Location: alternatif.php");
+            exit();
+        } else {
+            echo "Error deleting record: " . mysqli_error($dbcon);
+        }
+    } else {
+        echo "Error deleting related data: " . mysqli_error($dbcon);
+    }
+} else {
+    echo "Invalid request";
+}
+?>
